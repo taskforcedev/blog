1af3a21630d3b9ca67edb7c616c28b19e352f44e
David Craig <david.a.craig@accenture.com>
2015-12-02 14:46:48 +0000

    Add conditional routes depending on value of config

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,4 +1,20 @@
 <?php
 
 Route::group(['namespace' => 'Taskforcedev\Blog\Http\Controllers'], function() {
+
+    $location = config('taskforce-blog.location');
+
+    /* Config Conditional Routes */
+    switch ($location)
+    {
+        case 'home':
+            Route::get('/', ['as' => 'blog.index', 'uses' => 'BlogController@blog']);
+            break;
+        case 'blog':
+            Route::get('blog', ['as' => 'blog.index', 'uses' => 'BlogController@blog']);
+            break;
+        default:
+            break;
+    }
+
 });
