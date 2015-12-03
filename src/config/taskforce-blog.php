9e6e937d70b450c0d901f84c23355fd6a38398ff
PrivateSniper <privatesniper@gmail.com>
2015-12-03 11:42:01 +0000

    Add post-layout to config options.

--- src/config/taskforce-blog.php
+++ src/config/taskforce-blog.php
@@ -2,31 +2,40 @@
 
 return [
     /*
-    | -------------
-    | Blog Location
-    | -------------
-    |
-    | Allows you to choose where the blog should be located on your site
-    |
-    | Allowed Values:
-    |  - 'home': example.com - This will be your blog url.
-    |  - 'blog': example.com/blog - Blog will be located in the /blog route.
+    * Blog Location
+    * =============
+    * Allows you to choose where the blog should be located on your site
+    *
+    * Valid Options :-
+    *  "home": example.com - This will be your blog url.
+    *  "blog": example.com/blog - Blog will be located in the /blog route.
     */
 
-    'location' => 'home',
+    "location" => "home",
 
     /*
-    | ---------
-    | Framework
-    | ---------
-    |
-    | Provides the ability to chose which css classes are added to elements, full
-    | documentation can be found in the README
-    |
-    | Valid Options
-    | 'bootstrap-3', 'bootstrap-4', 'foundation-5', 'none', null
+    * Framework
+    * =========
+    *
+    * Provides the ability to chose which css classes are added to elements, full
+    * documentation can be found in the README
+    *
+    * Valid Options :-
+    * "bootstrap-3", "bootstrap-4", "foundation-5", "none", null
     */
 
-    'framework' => 'bootstrap-4',
+    "framework" => "bootstrap-4",
 
+    /*
+     * Blog Post Layout
+     * ================
+     * This setting allows you to choose how blog posts will be laid out on the site.
+     *
+     * Valid Options :-
+     *   "card" - All posts will be displayed as rows of cards - useful if you use featured images (nice for bootstrap 4).
+     *   "list" - All posts will go down the page in a list.
+     *   "none" - Your posts will be output as is, you can use this if you plan to implement your own options.
+     */
+
+    "post-layout" => "list",
 ];
