c497873d8e399b72409d3f3534d8f0a11931179c
PrivateSniper <privatesniper@gmail.com>
2015-12-03 10:32:44 +0000

    Add table and fillable properties.

--- src/Models/Post.php
+++ src/Models/Post.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $table = 'posts';
 
+    protected $fillable = [ 'title', 'author_id', 'body', 'status_id', 'image' ];
 }
