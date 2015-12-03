0e3eb92b47d76c402ed52003117272359c56de97
PrivateSniper <privatesniper@gmail.com>
2015-12-03 11:43:28 +0000

    Show if no posts are available.

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -1,4 +1,10 @@
 @extends($layout)
 
 @section('content')
+    @if(isset($posts) && count($posts) > 0)
+        @foreach($posts as $post)
+        @endforeach
+    @else
+        <p>There are currently no posts to view.</p>
+    @endif
 @endsection
