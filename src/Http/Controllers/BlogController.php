d11b641da249f42fb860c563bf16079e6ca7247a
PrivateSniper <privatesniper@gmail.com>
2015-12-03 09:56:49 +0000

    Implement index and post view initial logic.

--- src/Http/Controllers/BlogController.php
+++ src/Http/Controllers/BlogController.php
@@ -1,20 +1,47 @@
 <?php namespace Taskforcedev\Blog\Http\Controllers;
 
+use \Exception;
+use Taskforcedev\Blog\Models\Post;
+use Taskforcedev\Blog\Models\Status;
 use Taskforcedev\LaravelSupport\Http\Controllers\Controller;
 
 class BlogController extends Controller
 {
+    /**
+     * Blog index.
+     * @return mixed
+     */
     public function blog()
     {
         $data = $this->buildData();
+        $status = Status::getStatusByName('published');
 
-        return view('taskforce-blog::index', $data);
+        try {
+            $posts = Post::where('status_id', $status->id)->get();
+            $data['posts'] = $posts;
+            return view('taskforce-blog::index', $data);
+        } catch (Exception $e) {
+            $data['error'] = $e->getMessage(); // TODO: implement logic to only show this to admins.
+            return view('taskforce-blog::error', $data);
+        }
     }
 
-    public function viewPost()
+    /**
+     * View an individual post.
+     *
+     * @param integer $id Post Id.
+     *
+     * @return mixed
+     */
+    public function viewPost($id)
     {
         $data = $this->buildData();
 
-        return view('taskforce-blog::post', $data);
+        try {
+            $data['post'] = Post::where('id', $id);
+            return view('taskforce-blog::post', $data);
+        } catch (Exception $e) {
+            return view('taskforce-blog::404', $data);
+        }
     }
 }
