61cedf03056dc20fe06e2edc304e98d9ee0fe6d0
PrivateSniper <privatesniper@gmail.com>
2015-12-06 23:22:34 +0000

    Create foundation 6 blog index page.

---
+++ src/views/foundation6/index.blade.php
@@ -0,0 +1,26 @@
+@extends($layout)
+
+@section('content')
+    @if(isset($posts) && count($posts) > 0)
+        @foreach($posts as $post)
+            <div class="row">
+                <div class="blog-post">
+                    <div class="blog-post">
+                        <h1>{{ $post->title }} <small>- {{ $post->created_at }}</small></h1>
+                        <!-- img class="thumbnail" src="http://placehold.it/850x350" -->
+                        {{ $post->body }}
+                        <div class="callout">
+                            <ul class="menu simple">
+                                <li><a href="#">Author: {{ $post->author->name }}</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    @else
+        <div class="row">
+            <p>There are currently no posts to view.</p>
+        </div>
+    @endif
+@endsection
