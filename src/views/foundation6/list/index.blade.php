0d054be6a7891020962b7380c8f4561018ea8a6f
PrivateSniper <privatesniper@gmail.com>
2015-12-06 23:48:03 +0000

    Add columns classes (12 for now).

--- src/views/foundation6/list/index.blade.php
+++ src/views/foundation6/list/index.blade.php
@@ -4,15 +4,17 @@
     @if(isset($posts) && count($posts) > 0)
         @foreach($posts as $post)
             <div class="row">
-                <div class="blog-post">
+                <div class="medium-12 columns">
                     <div class="blog-post">
-                        <h1>{{ $post->title }} <small>- {{ $post->created_at }}</small></h1>
-                        <!-- img class="thumbnail" src="http://placehold.it/850x350" -->
-                        {{ $post->body }}
-                        <div class="callout">
-                            <ul class="menu simple">
-                                <li><a href="#">Author: {{ $post->author->name }}</a></li>
-                            </ul>
+                        <div class="blog-post">
+                            <h1>{{ $post->title }} <small>- {{ $post->created_at }}</small></h1>
+                            <!-- img class="thumbnail" src="http://placehold.it/850x350" -->
+                            {{ $post->body }}
+                            <div class="callout">
+                                <ul class="menu simple">
+                                    <li><a href="#">Author: {{ $post->author->name }}</a></li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
