738c1942892e2c260e50b1e9b9151b7cba421586
PrivateSniper <privatesniper@gmail.com>
2015-12-09 13:01:50 +0000

    Add option for /news to be blog base.

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,7 +2,37 @@
 
 Route::group(['namespace' => 'Taskforcedev\Blog\Http\Controllers'], function() {
 
+    /* Get the route base from configuration */
+
     $location = config('taskforce-blog.location', 'home');
+    $blogBase = "/";
+    /* Config Conditional Routes */
+    switch ($location) {
+        case 'home':
+            $blogBase = '/';
+            break;
+        case 'blog':
+            $blogBase = '/blog/';
+            break;
+        case 'news':
+            $blogBase = '/news/';
+            break;
+        default:
+            break;
+    }
+
+    /* PUBLIC ROUTES */
+
+    Route::get($blogBase, ['as' => 'blog.index', 'uses' => 'BlogController@blog']);
+    Route::get($blogBase . 'post/{$id}', ['as' => 'blog.post.view', 'uses' => 'BlogController@viewPost']);
+
+    /* ADMIN */
+    Route::get('admin/blog', ['as' => 'blog.admin.index', 'uses' => 'AdminController@index']);
+    Route::get('admin/blog/post', ['as' => 'blog.admin.index', 'uses' => 'AdminController@postForm']);
+
+    Route::post('admin/blog/post', ['as' => 'blog.post.create', 'uses' => 'AdminController@createPost']);;
+
+    /* SYNDICATION */
 
     $rss = config('taskforce-blog.feeds.rss', true);
     $atom = config('taskforce-blog.feeds.atom', true);
@@ -14,24 +44,4 @@ Route::group(['namespace' => 'Taskforcedev\Blog\Http\Controllers'], function() {
     if ($atom) {
         Route::get('feed.atom', ['as' => 'blog.atom.index', 'uses' => 'BlogController@blogAtom']);
     }
-
-    /* Config Conditional Routes */
-    switch ($location) {
-        case 'home':
-            Route::get('/', ['as' => 'blog.index', 'uses' => 'BlogController@blog']);
-            Route::get('post/{$id}', ['as' => 'blog.post.view', 'uses' => 'BlogController@viewPost']);
-            break;
-        case 'blog':
-            Route::get('blog', ['as' => 'blog.index', 'uses' => 'BlogController@blog']);
-            Route::get('blog/post/{$id}', ['as' => 'blog.post.view', 'uses' => 'BlogController@viewPost']);
-            break;
-        default:
-            break;
-    }
-
-    /* Admin Routes */
-    Route::get('admin/blog', ['as' => 'blog.admin.index', 'uses' => 'AdminController@index']);
-    Route::get('admin/blog/post', ['as' => 'blog.admin.index', 'uses' => 'AdminController@postForm']);
-
-    Route::post('admin/blog/post', ['as' => 'blog.post.create', 'uses' => 'AdminController@createPost']);;
 });
